05f5ffd5b14bce9a7ac1da1ef0c506d22bf77f3b
mhamsha <hamza.shahzad.8692@gmail.com>
2024-07-06 21:57:48 +0500

    refactor: add site key to env variables

--- conf/conf.js
+++ conf/conf.js
@@ -5,5 +5,6 @@ const conf = {
   appwriteBucketID: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
   appwriteDatabaseID: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
   tinymceApiKey:String(import.meta.env.VITE_TINYMCE_API_KEY),
+  cloudFlareSiteKey:String(import.meta.env.VITE_CLOUDFLARE_SITE_KEY),
 };
 export default conf;
